345fdcc1b22ea2345d7901431bdf8150c1affc83
Tauane Costa <tauanedasilvacosta@gmail.com>
2021-08-01 13:44:03 -0300

    first commit

---
+++ src/main/java/com/pw2/jpa/HelloController.java
@@ -0,0 +1,18 @@
+package com.pw2.jpa;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ *
+ */
+@Path("/hello")
+@Singleton
+public class HelloController {
+
+    @GET
+    public String sayHello() {
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/pw2/jpa/JpaRestApplication.java
@@ -0,0 +1,11 @@
+package com.pw2.jpa;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class JpaRestApplication extends Application {
+}
